85da9e190824bffd41f3fb69f101ce4dd784e475
shubhsudan <108739113+shubhsudan@users.noreply.github.com>
2023-04-01 11:56:38 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,34 @@
+//HERE WE USE get() method for getting the output of the student details
+
+public class Student {
+    private String name;
+    private String dob;
+    private int prn;
+    public Student(String name, String dob, int prn) {
+        this.name = name;
+        this.dob = dob;
+        this.prn = prn;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDob() {
+        return dob;
+    }
+    public int getPrn() {
+        return prn;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+    public void setPrn(int prn) {
+        this.prn = prn;
+    }
+    @Override
+    public String toString() {
+        return "Name: " + name + ", DOB: " + dob + ", PRN: " + prn;
+    }
+}
